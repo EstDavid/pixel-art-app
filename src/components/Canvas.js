f1390754669af2d2dd15f80545cef02228a33b7f
Vandenynas <stlo.subs@gmail.com>
2023-01-25 21:22:47 +0100

    Extracted PixelsRow and Pixel components

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,44 +1,13 @@
-const Row = ({ columns, onClick, rowNumber, color, showGrid }) => {
+import PixelsRow from "./canvas/PixelsRow"
 
-    // The row element uses the h-auto bootstrap property to give all rows equal 
-    // height inside the main canvas row
-    // Padding and margin need to be set to 0
-    return (
-        <div className="row h-auto p-0 m-0">
-            {columns.map((row, index) =>
-                <Pixel 
-                    key={index}
-                    showGrid={showGrid}
-                    color={color}
-                    onClick={onClick}
-                    rowNumber={rowNumber}
-                    columnNumber={index} />
-            )}
-        </div>
-    )
-}
-
-const Pixel = ({ showGrid, color, onClick, rowNumber, columnNumber }) => {
-    // Each col element uses the w-auto bootstrap property to give all columns equal 
-    // width inside each row
-    // Padding and margin need to be set to 0
-    return (
-        <div
-            id={`${rowNumber}-${columnNumber}`}
-            className={`col w-auto p-0 m-0 ${showGrid ? 'border border-dark' : null}`}
-            style={{ backgroundColor: `rgba(${color[rowNumber][columnNumber]})`,
-        borderStyle: 'none' }}
-            onClick={onClick} >
-        </div>
-    )
-}
-
+// Component that contains the image and is made of rows of Pixel components
 const Canvas = ({
         showGrid,
         colorMatrix,
         handlePixelClick,
     }) => {
 
+    // Getting the number of rows and columns from the size of the matrix
     const rows = colorMatrix.length
     const columns = colorMatrix[0].length
 
@@ -60,12 +29,24 @@ const Canvas = ({
     //    - A 'row' element which contains all the rows
     //    - Each row contains all the individual 'Pixel' components
         return(
-        <div id="canvas" className="col-8 p-2 border border-dark">
-            {// The aspectRatio css property ensures that the main canvas row will have equal height as the width
+        <div id="canvas" className="col-12 col-lg-4 p-2">
+            {// The aspect-ratio css property ensures that the main canvas row will have equal height as the width
             // imposed by the bootstrap grid layout
+            // This property is applied in the .canvas-container class
             }
-            <div className="row m-0 border border-dark" style={{aspectRatio: '1', backgroundColor: `rgba(255,0,0,0.1)`}}>
-                {rowsArray.map((row, index) => <Row key={index} showGrid={showGrid} columns={columnsArray} onClick={handlePixelClick} rowNumber={index} color={colorMatrix} />)}
+            <div className="row m-0 p-0 border border-dark canvas-container">
+                {rowsArray.map((row, index) => {
+                    return (
+                        <PixelsRow
+                            key={index}
+                            showGrid={showGrid}
+                            columns={columnsArray}
+                            onClick={handlePixelClick}
+                            rowNumber={index}
+                            color={colorMatrix}
+                        />)}
+                    )
+                }
             </div>
         </div>
     )
