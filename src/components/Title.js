6050e040fece3b892e5c9b7540bc3beaaa6da055
Vandenynas <stlo.subs@gmail.com>
2023-01-26 20:03:34 +0100

    Changed the Brand text to stretch it to the height of the logo

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,19 +3,19 @@ import Logo from '../assets/logo.png'
 // Title component using a bootstrap .navbar and .navbar-brand component
 const Title = () => {
     return (
-        <nav className="navbar ">
+        <nav className="navbar">
             <div className="container">
-                <a className="navbar-brand fs-1 p-2 align-items-bottom" href="#">
+                <a className="navbar-brand fs-1 p-2 d-flex" href="#!">
                     <img 
                         src={Logo}
                         alt="Logo"
                         width="90"
                         height="auto"
-                        className="d-inline-block align-text-bottom rounded" />
-                        <span className="ms-2 px-3 bg-dark rounded">
-                            <strong className="pixel">Pixel </strong>
-                            <strong className="artifier">Artifier</strong>
-                        </span>
+                        className="rounded" />
+                    <span className="d-flex align-items-center ms-2 px-3 bg-dark rounded pixel-artifier-brand">
+                        <strong className="pixel">Pixel</strong>
+                        <strong className="artifier">Artifier</strong>
+                    </span>
                 </a>
             </div>
         </nav>
